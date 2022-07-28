fbb4d7b820451dd49ba7492d692b49429e7c1e10
Alexander2119m <105405177+Alexander2119m@users.noreply.github.com>
2022-07-28 13:18:48 -0500

    Add files via upload

---
+++ Examenpoo/ClassIngredientes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examenpoo
+{
+    public class ClassIngredientes: ClassHelado, Decorador
+    {
+        public string vainilla { get; set; }
+
+        public int choco { get; set; }
+
+        public int banano { get; set; }
+
+    }
+    public ClassIngredientes(string vainilla, string choco, double banano) : base (string helado, string Ingre) 
+    {
+        this.vainilla = vainilla;
+        this.choco = choco;
+        this.banano = banano;
+    }
+    public void Calcular()
+    {
+        int resultado = 0;
+        Console.WriteLine("Ingrediente vainilla");
+        Console.WriteLine("Ingrediente choco");
+        Console.WriteLine("Ingrediente banano");
+        resultado = vainilla + 1;
+
+    }
+
+
+
+}
---
+++ Examenpoo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examenpoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
